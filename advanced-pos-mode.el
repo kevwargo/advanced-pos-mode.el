65845ce1d080a5e2893b5083372f1796589f12e3
kevwargo <jarasz.kevwargo@gmail.com>
2025-07-28 23:31:22 +0200

    Implement proper extending of line-number-mode position in mode-line

--- advanced-pos-mode.el
+++ advanced-pos-mode.el
@@ -5,17 +5,24 @@
   "Minor mode for showing extra info about current buffer position"
   :global t
   :lighter nil
-  )
+  (setq mode-line-position
+        (mapcar #'advanced-pos--wrap-ln mode-line-position)))
 
-;; (setq mode-line-position
-;;       '(25 ("%l:%c " (:eval
-;;                   (if (eobp)
-;;                       (format "(%d: EOF)" (point))
-;;                     (let ((c (char-after)))
-;;                       (concat (format "(%d: #%x %d " (point) c c)
-;;                               (propertize (if (= (char-after) ?%) "%%" (format "%c" c))
-;;                                           'face 'help-key-binding)
-;;                               ")")))))))
+(defun advanced-pos--wrap-ln (elt)
+  "This function wraps top-level `line-number-mode' element inside `mode-line-position'.
+Other elements of `mode-line-position' are returned unchanged."
+  (if (eq (car-safe elt) 'line-number-mode)
+      `(advanced-pos-mode
+        (25 (" %l:%c " (:eval
+                        (if (eobp)
+                            (format "(%d: EOF)" (point))
+                          (let ((c (char-after)))
+                            (concat (format "(%d: #%x %d " (point) c c)
+                                    (propertize (if (= (char-after) ?%) "%%" (format "%c" c))
+                                                'face 'help-key-binding)
+                                    ")"))))))
+        ,elt)
+    elt))
 
 (provide 'advanced-pos-mode)
 
